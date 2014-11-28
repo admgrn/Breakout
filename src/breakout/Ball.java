8a5dea3ced8b428b60ef884f08e72edaf0321eca
Adam Greenstein <adamgreenstein@comcast.net>
2014-11-28 11:25:07 -0500

    Added ball

--- src/breakout/Ball.java
+++ src/breakout/Ball.java
@@ -1,23 +1,35 @@
 package breakout;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class Ball extends JPanel implements PositionInterval {
     private int diameter;
 
     private Position position;
+    private BufferedImage image;
 
     public Ball(int diameter) {
         super();
         this.diameter = diameter;
         this.position = new Position(0, 0);
+
+        try {
+            image = ImageIO.read(Ball.class.getResource("../images/ball.png"));
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
+
         setOpaque(false);
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.fillOval(position.getX(), position.getY(), diameter, diameter);
+        //g.fillOval(position.getX(), position.getY(), diameter, diameter);
+        ((Graphics2D)g).drawImage(image, null, position.getX(), position.getY());
     }
 
     public void UpdatePosition(int dx, int dy) {
